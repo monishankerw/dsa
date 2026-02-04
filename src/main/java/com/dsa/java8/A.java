871f1ebd8eec9b7a23d6e0fe9a7709fd26fe0d1d
monishanker <monishnker@gmail.com>
2026-02-04 22:21:52 +0530

    c

--- src/main/java/com/dsa/java8/A.java
+++ src/main/java/com/dsa/java8/A.java
@@ -1,7 +1,4 @@
 package com.dsa.java8;
-
-import com.dsa.arrays.basic.PrimeNumber;
-
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -16,65 +13,65 @@ public class A {
 //        List<Integer> primeNum = list.stream().filter(PrimeNumber::isPrimes).toList();
 //        System.out.println("Prime number"+primeNum);
         List<Integer> evenNumber = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
-        System.out.println("Even Number.."+evenNumber);
+        System.out.println("Even Number.." + evenNumber);
 
-        Map<Boolean,List<Integer>> evenOrOdd=list.stream().collect(Collectors.partitioningBy(x->x%2==0));
-        System.out.println("Even:"+evenOrOdd.get(true));
-        System.out.println("Odd:"+evenOrOdd.get(false));
+        Map<Boolean, List<Integer>> evenOrOdd = list.stream().collect(Collectors.partitioningBy(x -> x % 2 == 0));
+        System.out.println("Even:" + evenOrOdd.get(true));
+        System.out.println("Odd:" + evenOrOdd.get(false));
         Integer sumOfEvenNumber = list.stream().filter(x -> x % 2 == 0).reduce(0, (a, b) -> a + b);
-        System.out.println("Sum Of Even Number.."+sumOfEvenNumber);
+        System.out.println("Sum Of Even Number.." + sumOfEvenNumber);
         Integer sumOfInteger = list.stream().reduce(0, Integer::sum);
-        System.out.println("Sum Of Integer.."+sumOfInteger);
-        list.stream().mapToInt(x->x).max().ifPresent(max-> System.out.println("Max:"+max));
-        list.stream().mapToInt(x->x).min().ifPresent(min-> System.out.println("Min:"+min));
+        System.out.println("Sum Of Integer.." + sumOfInteger);
+        list.stream().mapToInt(x -> x).max().ifPresent(max -> System.out.println("Max:" + max));
+        list.stream().mapToInt(x -> x).min().ifPresent(min -> System.out.println("Min:" + min));
         int sum = list.stream().mapToInt(Integer::intValue).sum();
         System.out.println(sum);
         //Second highest Number
         Optional<Integer> first1 = list.stream().sorted(Comparator.reverseOrder()).skip(1).findFirst();
-        System.out.println("Second highest elements"+first1);
+        System.out.println("Second highest elements" + first1);
         List<Integer> list6 = list.stream().filter(x -> x % 2 == 0).limit(6).toList();
         System.out.println(list6);
         Integer sumOfEvenAndSquare = list.stream().filter(x -> x % 2 == 0).map(x -> x * x).reduce(0, Integer::sum);
-        System.out.println("Sum Of Even and sqare.."+sumOfEvenAndSquare);
+        System.out.println("Sum Of Even and sqare.." + sumOfEvenAndSquare);
         OptionalDouble average = list.stream().filter(x -> x % 2 == 0).mapToInt(x -> x).average();
-        System.out.println("Average..."+average);
+        System.out.println("Average..." + average);
         List<Integer> duplicate = list.stream().distinct().collect(Collectors.toList());
-        System.out.println("Duplicate.."+duplicate);
+        System.out.println("Duplicate.." + duplicate);
         long count = list.stream().count();
-        System.out.println("Count{}"+count);
+        System.out.println("Count{}" + count);
         long divBy = list.stream().mapToInt(x -> x).filter(x -> x % 3 == 0).count();
-        System.out.println("Siv by 3.."+divBy);
+        System.out.println("Siv by 3.." + divBy);
         //Accending
         List<Integer> list1 = list.stream().sorted().toList();
-        System.out.println("Sorting Accending Order"+list1);
+        System.out.println("Sorting Accending Order" + list1);
         List<Integer> list2 = list.stream().sorted(Comparator.reverseOrder()).toList();
-        System.out.println("Sorting Decending Order"+list2);
+        System.out.println("Sorting Decending Order" + list2);
         List<List<Integer>> list3 = Arrays.asList(Arrays.asList(1, 2, 3), Arrays.asList(2, 3, 5), Arrays.asList(6, 7, 3));
         List<Integer> merge = list3.stream().flatMap(List::stream).toList();
-        System.out.println("Merge Elements"+merge);
+        System.out.println("Merge Elements" + merge);
         Stream<Integer> distinct = list3.stream().flatMap(List::stream).distinct();
-        System.out.println("DDDD"+distinct);
+        System.out.println("DDDD" + distinct);
 
 //Convert a list of number to ASCII Character
         List<Integer> list4 = Arrays.asList(54, 64, 65, 34, 75, 87);
-        list4.stream().mapToInt(x->x).mapToObj(c->(char)c).forEach(c-> System.out.println("CHAR"+c));
-        int[]arr={1,2,3,4,5,2};
-        Arrays.stream(arr).distinct().forEach(x-> System.out.println("Remove dup"+x));
-        int[]arr1={1,2,1,3,2,1,3,4,2};
-        int k=2;
+        list4.stream().mapToInt(x -> x).mapToObj(c -> (char) c).forEach(c -> System.out.println("CHAR" + c));
+        int[] arr = {1, 2, 3, 4, 5, 2};
+        Arrays.stream(arr).distinct().forEach(x -> System.out.println("Remove dup" + x));
+        int[] arr1 = {1, 2, 1, 3, 2, 1, 3, 4, 2};
+        int k = 2;
         Map<Integer, Long> FreOCC = Arrays.stream(arr1).boxed().collect(Collectors.groupingBy(x -> x, Collectors.counting()));
-        System.out.println("Fre OCC"+FreOCC);
-        Map<Integer, java.lang.Long>map =Arrays.stream(arr1).boxed().collect(Collectors.groupingBy(x->x,Collectors.counting()));
+        System.out.println("Fre OCC" + FreOCC);
+        Map<Integer, java.lang.Long> map = Arrays.stream(arr1).boxed().collect(Collectors.groupingBy(x -> x, Collectors.counting()));
         List<Integer> list5 = map.entrySet().stream().filter(e -> e.getValue() == k).map(Map.Entry::getKey).toList();
-        System.out.println("Kth Freq Occ"+list5);
+        System.out.println("Kth Freq Occ" + list5);
         Optional<Map.Entry<Integer, Long>> first = Arrays.stream(arr1).boxed().collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting())).entrySet().stream().filter(e -> e.getValue() == 1).findFirst();
-        System.out.println("Find Non repeating number"+first);
-        Map<Object, Long> collect = Arrays.stream(arr1).boxed().collect(Collectors.groupingBy(x ->x, Collectors.counting()));
-        collect.forEach((k1,v)-> System.out.println(k+"->"+v));
+        System.out.println("Find Non repeating number" + first);
+        Map<Object, Long> collect = Arrays.stream(arr1).boxed().collect(Collectors.groupingBy(x -> x, Collectors.counting()));
+        collect.forEach((k1, v) -> System.out.println(k + "->" + v));
         //Show Duplicate
         HashSet<Object> set = new HashSet<>();
         Set<Integer> set1 = Arrays.stream(arr1).filter(x -> !set.add(x)).boxed().collect(Collectors.toSet());
-        System.out.println("Show duplicate"+set1);
+        System.out.println("Show duplicate" + set1);
 
         List<String> upperCase = word.stream().map(String::toUpperCase).toList();
         System.out.println(upperCase);
@@ -83,33 +80,38 @@ public class A {
         Map<Integer, List<String>> grouplength = word.stream().collect(Collectors.groupingBy(String::length));
         System.out.println(grouplength);
         long count1 = word.stream().filter(x -> x.length() > 3).count();
-        System.out.println("Count Length::"+count1);
+        System.out.println("Count Length::" + count1);
         List<String> sort = word.stream().sorted().toList();
-        System.out.println("Sort alph"+sort);
-        word.stream().mapToInt(String::length).forEach(l-> System.out.println(l));
+        System.out.println("Sort alph" + sort);
+        word.stream().mapToInt(String::length).forEach(l -> System.out.println(l));
         Optional<String> reduce = word.stream().reduce((x, y) -> x.length() > y.length() ? x : y);
         System.out.println(reduce);
 
         //Frequency occurence
-        String input="aavmprqounba";
-        String []str=input.split(" ");
+        String input = "aavmprqounba";
+        String[] str = input.split(" ");
         Map<String, Long> freqOcc = Arrays.stream(str).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        System.out.println("Freq Occ"+freqOcc);
+        System.out.println("Freq Occ" + freqOcc);
         //Count character occurrence  in a string
-        String inputs="hello word";
+        String inputs = "hello word";
         Map<Character, Long> map1 = inputs.chars().mapToObj(c -> (char) c).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        System.out.println("Count character occurrence  in a string"+map1);
+        System.out.println("Count character occurrence  in a string" + map1);
         //Frequence occ word
-        String str1="Test I Test my Test abc";
+        String str1 = "Test I Test my Test abc";
         String[] words = str1.split("\\s+");
         Map<String, Long> freqOccWords = Arrays.stream(words).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         System.out.println(freqOccWords);
-
+        //All Repeated word
+        Arrays.stream(str1.toLowerCase().split("\\s+")).collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting())).entrySet().stream().filter(e -> e.getValue() == 1).map(Map.Entry::getKey).findFirst();
+        // Maximum occurence character
+        input.chars().mapToObj(c -> (char) c).collect(Collectors.groupingBy(c -> c, Collectors.counting())).entrySet().stream().max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse(' ');
+        // All Non Repeated Word
+        Arrays.stream(str1.toLowerCase().split("\\s+")).collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting())).entrySet().stream().filter(e -> e.getValue() == 1).map(Map.Entry::getKey).forEach(System.out::println);
         //Freq Occ Character
-        char[]c={'a','a','d','w','t','q','t'};
-        new String(c).chars().mapToObj(c1->(char)c1).collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+        char[] c = {'a', 'a', 'd', 'w', 't', 'q', 't'};
+        new String(c).chars().mapToObj(c1 -> (char) c1).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         //Find duplicate charcters woth occurence>1
-        List<String> dupGraterThan1 = Arrays.stream(words).collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream().filter(e -> e.getValue() >1).map(Map.Entry::getKey).toList();
+        List<String> dupGraterThan1 = Arrays.stream(words).collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream().filter(e -> e.getValue() > 1).map(Map.Entry::getKey).toList();
         System.out.println(dupGraterThan1);
 
         //Find unique elements
@@ -120,11 +122,36 @@ public class A {
         Optional<String> firstNonRepeating = Arrays.stream(words).collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream().filter(e -> e.getValue() == 1).map(Map.Entry::getKey).findFirst();
         System.out.println(firstNonRepeating);
 
+        // Index of first Repeated char
+        String strs = "swiss";
+        String str11 = strs.toLowerCase();
+
+        Optional<Character> firstRepeatedChar = str11.chars().mapToObj(c1 -> (char) c1).collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting())).entrySet().stream().filter(e -> e.getValue() > 1).map(Map.Entry::getKey).findFirst();
+
+        if (firstRepeatedChar.isPresent()) {
+            int index = str11.indexOf(firstRepeatedChar.get());
+            System.out.println("First repeated char: " + firstRepeatedChar.get());
+            System.out.println("Index: " + index);
+        } else {
+            System.out.println("No repeated character found");
+        }        //Anagram String
+        String s1 = "listen";
+        String s2 = "slient";
+        boolean isAnagram = charFrequency(s1).equals(charFrequency(s2));
+
+        System.out.println("Anagram: " + isAnagram);
+
+
     }
-    public static boolean isPrimes(int x){
-        int n=0;
-        for (int i=2;i<x/2;i++){
-            if(n%2==0){
+
+    private static Map<Character, Long> charFrequency(String str) {
+        return str.chars().mapToObj(c -> (char) c).collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+    }
+
+    public static boolean isPrimes(int x) {
+        int n = 0;
+        for (int i = 2; i < x / 2; i++) {
+            if (n % 2 == 0) {
                 return false;
             }
         }
